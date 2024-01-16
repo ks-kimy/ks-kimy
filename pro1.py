6cd697b715fb1b0bc829babde3064d21e3720894
kks <kkisung2657@gmail.com>
2024-01-16 09:45:32 +0900

    answer

--- pro1.py
+++ pro1.py
@@ -8,23 +8,17 @@ for tc in range(1, T + 1):
     # matrix = []
     N = int(input())
     max=0
-    # for _ in range(N):
-    row = list(map(int, input().split()))
-    # matrix.append(row)
-    score_dict = {}
+    max_key = 0
+    dict_score ={}
+    row = list(map(int, input().split())) # 
     for i in range(0,101):
-        score_dict[i] = 0 # i 점수의 사람 수
-        
-        for j in range(1,1001):
-            if i == row[j-1]:
-                score_dict[i] += 1
-        
-        if score_dict[i] >= max:
-            max = score_dict[i]
+        dict_score[i]= 0
+        for j in range(0,1000):
+            if i == row[j]:
+                dict_score[i] += 1
+        if dict_score[i]>= max:
+            max = dict_score[i]
+            max_key = i
+    print(f"#{tc}", max_key)
     
-    print(max)
-        # print(score_dict)
-    # print(f"#{tc} {mode_value}")
-    # pass
-
-    # print(f'#{tc}')
+    
